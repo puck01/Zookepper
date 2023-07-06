07f461f6df5e61f97dc4c06cd798c99dba1d6ad6
Puck <136465738+puck01@users.noreply.github.com>
2023-07-06 18:14:27 +0530

    Add files via upload

---
+++ Zookeeper/src/Stage1.java
@@ -0,0 +1,11 @@
+public class Stage1 {
+    public static void main(String[] args) {
+        System.out.println("""
+                I love animals!
+                Let's check on the animals...
+                The deer looks fine.
+                The bat looks happy.
+                The lion looks healthy.
+                """);
+    }
+}
---
+++ Zookeeper/src/Stage3.java
@@ -0,0 +1,153 @@
+import java.util.Scanner;
+
+public class Stage3 {
+    public static void main(String[] args) {
+        String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""";
+
+        String lion = """
+            Switching on the camera in the lion habitat...
+                                                           ,w.
+                                                         ,YWMMw  ,M  ,
+                                    _.---.._   __..---._.'MMMMMw,wMWmW,
+                               _.-""        '''           YP"WMMMMMMMMMb,
+                            .-' __.'                   .'     MMMMW^WMMMM;
+                _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+             ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+            ,MM:.    .'.-'   .'     ;     `\\    ;     `,     MMMMMMMW `"=./`-,
+            WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+            "^MP__.-'    ,-' _.--""   `-,   ;       \\  ; ;MMMMMMMMMMW^``; __|
+                       /   .'            ; ;         )  )`{  \\ `"^W^`,   \\  :
+                      /  .'             /  (       .'  /     Ww._     `.  `"
+                     /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+                    (--, )                `,_ / `) \\/"")      ^"      `-, -;"\\:
+            The lion is roaring!""";
+
+        String deer = """
+            Switching on the camera in the deer habitat...
+               /|       |\\
+            `__\\       //__'
+               ||      ||
+             \\__`\\     |'__/
+               `_\\   //_'
+               _.,:---;,._
+               \\_:     :_/
+                 |@. .@|
+                 |     |
+                 ,\\.-./ \\
+                 ;;`-'   `---__________-----.-.
+                 ;;;                         \\_\\
+                 ';;;                         |
+                  ;    |                      ;
+                   \\   \\     \\        |      /
+                    \\_, \\    /        \\     |\\
+                      |';|  |,,,,,,,,/ \\    \\ \\_
+                      |  |  |           \\   /   |
+                      \\  \\  |           |  / \\  |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       |_||_|           |_|   |_|
+                      /_//_/           /_/   /_/
+            Our 'Bambi' looks hungry. Let's go to feed it!""";
+
+        String goose = """
+            Switching on the camera in the goose habitat...
+            
+                                                _
+                                            ,-"" "".
+                                          ,'  ____  `.
+                                        ,'  ,'    `.  `._
+               (`.         _..--.._   ,'  ,'        \\    \\
+              (`-.\\    .-""        ""'   /          (  d _b
+             (`._  `-"" ,._             (            `-(   \\
+             <_  `     (  <`<            \\              `-._\\
+              <`-       (__< <           :
+               (__        (_<_<          ;
+                `------------------------------------------
+            The goose is staring intently at you... Maybe it's time to change the channel?""";
+
+        String bat = """
+            Switching on the camera in the bat habitat...
+            _________________               _________________
+             ~-.              \\  |\\___/|  /              .-~
+                 ~-.           \\ / o o \\ /           .-~
+                    >           \\  W  //           <
+                   /             /~---~\\             \\
+                  /_            |       |            _\\
+                     ~-.        |       |        .-~
+                        ;        \\     /        i
+                       /___      /\\   /\\      ___\\
+                            ~-. /  \\_/  \\ .-~
+                               V         V
+            This bat looks like it's doing fine.""";
+
+        String rabbit = """
+            Switching on the camera in the rabbit habitat...
+                     ,
+                    /|      __
+                   / |   ,-~ /
+                  Y :|  //  /
+                  | jj /( .^
+                  >-"~"-v"
+                 /       Y
+                jo  o    |
+               ( ~T~     j
+                >._-' _./
+               /   "~"  |
+              Y     _,  |
+             /| ;-"~ _  l
+            / l/ ,-"~    \\
+            \\//\\/      .- \\
+             Y        /    Y
+             l       I     !
+             ]\\      _\\    /"\\
+            (" ~----( ~   Y.  )
+            It looks like we will soon have more rabbits!""";
+
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the number of the habitat you would like to view:");
+        int num = sc.nextInt();
+        switch(num){
+            case 0:
+                System.out.println(camel);
+                break;
+            case 1:
+                System.out.println(lion);
+                break;
+            case 2:
+                System.out.println(deer);
+                break;
+            case 3:
+                System.out.println(goose);
+                break;
+            case 4:
+                System.out.println(bat);
+                break;
+            case 5:
+                System.out.println(rabbit);
+                break;
+        }
+        System.out.println("---");
+        System.out.println("You've reached the end of the program. To check another habitat, please restart the watcher.");
+    }
+}
